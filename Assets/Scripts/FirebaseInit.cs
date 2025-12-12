055b624dce7c58dabb5bf3357019c26af3c1072e
paul991006 <1099paul@gmail.com>
2025-12-12 20:26:58 +0900

    12.12
    테스트

---
+++ Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,22 @@
+using Firebase;
+using Firebase.Auth;
+using UnityEngine;
+
+public class FirebaseInit : MonoBehaviour
+{
+    private void Awake()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            var status = task.Result;
+            if (status == DependencyStatus.Available)
+            {
+                Debug.Log("Firebase 초기화 성공!");
+            }
+            else
+            {
+                Debug.LogError($"Firebase 초기화 실패: {status}");
+            }
+        });
+    }
+}
